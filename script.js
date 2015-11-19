232e9a571fb93e0a887152de35adbe259fcfbab9
Zoe Wood <z.e.wood22@gmail.com>
2015-11-18 20:34:57 -0500

    testing get request

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-*test*
+$.ajax({
+	type: "GET", 
+	dataType: "jsonp",
+	cache: false, 
+	url: "https://api.instagram.com/v1/media/popular?access_token=1075336788.0a2acbd.98943cbf4457430c9ae361dfa590ee24",
+	success: function(data){
+		for(var i = 0; i < 6; i++){
+			 $(".popular").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></li>"); 
+		}
+	}
+});
