6080a51c0c7500f7b72dac29ef887fa67b66a930
Zoe Wood <z.e.wood22@gmail.com>
2015-11-20 16:58:46 -0500

    writing search function

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
+/* Zoe Wood * InstaGroupings * HW3 * November 19, 2015 */
+
+/*test: getting popular images*/
+
+var d = 0;
+
+var searchInstagram = function(query){
+
+	var user = $("input[name='users']").val(); 
+
+}
+
+document.getElementById('search-form').addEventListener('submit', function (e) {
+	e.preventDefault(); 
+	searchInstagram(document.getElementById('query').value);
+}, false); 
+
 $.ajax({
-	type: "GET", 
-	dataType: "jsonp",
-	cache: false, 
-	url: "https://api.instagram.com/v1/media/popular?access_token=1075336788.0a2acbd.98943cbf4457430c9ae361dfa590ee24",
-	success: function(data){
-		for(var i = 0; i < 6; i++){
-			 $(".popular").append("<li><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url + "'></img></a></li>"); 
-		}
+  type: "GET",
+  dataType: "jsonp",
+	cache: false,
+  url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=1075336788.0a2acbd.98943cbf4457430c9ae361dfa590ee24",
+  success: function(data) {
+  		for(var i = 0; i < data["data"].length; i++)
+  		{
+  			var img = new Image(); 
+  			img.src = data["data"][i]["standard_resolution"];
+  			//img.src = data["data"][i]["user"]["profile_picture"]; 
+  			$("#put").append(img); 
+
+  			//$("#put").append(data["data"][i]["user"]["profile_picture"]);
+  		}
 	}
 });
+
